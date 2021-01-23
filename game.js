faeebd4e2b12e754b1dfe891b92ad6141ce43d83
Siddharth <siddharth.muscat@gmail.com>
2021-01-23 14:50:19 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,41 +1,195 @@
-const textElement = document.getElementById('text');
-const optionsElement = document.getElementById('option-buttons');
-let state = {};
-const textNodes = [];
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
 
 function startGame() {
-    state = {};
-    showTextNode(1);
+    state = {}
+    showTextNode(1)
 }
 
-function selectOption(option) {
-    const next = option.next;
-    state = Object.assign(state, option.setState);
-    showTextNode(next);
-}
+function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionButtonsElement.firstChild) {
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
 
-function showTextNode(nodeIndex) {
-    const node = textNodes.find(node => node.id === nodeIndex);
-    textElement.innerHTML = parseMarkdown(node.text);
-    while (optionsElement.firstChild) optionsElement.removeChild(optionsElement.firstChild);
-
-    node.options.forEach(option => {
+    textNode.options.forEach(option => {
         if (showOption(option)) {
-            const button = document.createElement('button');
-            button.innerHTML = parseMarkdown(option.text);
-            button.classList.add('btn');
-            button.addEventListener('click', function () { selectOption(option); });
-            optionsElement.appendChild(button);
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
         }
-    });
+    })
 }
 
 function showOption(option) {
-    return option.requiredState == null || option.requiredState(state);
+    return option.requiredState == null || option.requiredState(state)
 }
 
-startGame();
-
-function parseMarkdown(markdown) {
-    return markdown.replace(/^\s*\n\*/gm, '<ul>\n*').replace(/^(\*.+)\s*\n([^\*])/gm, '$1\n</ul>\n\n$2').replace(/^\*(.+)/gm, '<li>$1</li>').replace(/^\s*\n\d\./gm, '<ol>\n1.').replace(/^(\d\..+)\s*\n([^\d\.])/gm, '$1\n</ol>\n\n$2').replace(/^\d\.(.+)/gm, '<li>$1</li>').replace(/^\>(.+)/gm, '<blockquote>$1</blockquote>').replace(/[\#]{6}(.+)/g, '<h6>$1</h6>').replace(/[\#]{5}(.+)/g, '<h5>$1</h5>').replace(/[\#]{4}(.+)/g, '<h4>$1</h4>').replace(/[\#]{3}(.+)/g, '<h3>$1</h3>').replace(/[\#]{2}(.+)/g, '<h2>$1</h2>').replace(/[\#]{1}(.+)/g, '<h1>$1</h1>').replace(/^(.+)\n\=+/gm, '<h1>$1</h1>').replace(/^(.+)\n\-+/gm, '<h2>$1</h2>').replace(/\!\[([^\]]+)\]\(([^\)]+)\)/g, '<img src="$2" alt="$1" />').replace(/[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g, '<a href="$2" title="$4">$1</a>').replace(/[\*\_]{2}([^\*\_]+)[\*\_]{2}/g, '<b>$1</b>').replace(/[\*\_]{1}([^\*\_]+)[\*\_]{1}/g, '<i>$1</i>').replace(/[\~]{2}([^\~]+)[\~]{2}/g, '<del>$1</del>').replace(/^\s*\n\`\`\`(([^\s]+))?/gm, '<pre class="$2">').replace(/^\`\`\`\s*\n/gm, '</pre>\n\n').replace(/[\`]{1}([^\`]+)[\`]{1}/g, '<code>$1</code>').replace(/^\s*(\n)?(.+)/gm, function (m) { return /\<(\/)?(h\d|ul|ol|li|blockquote|pre|img)/.test(m) ? m : '<p>' + m + '</p>'; }).replace(/(\<pre.+\>)\s*\n\<p\>(.+)\<\/p\>/gm, '$1$2');
+function selectOption(option) {
+    const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0) {
+        return startGame()
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
 }
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'You wake up in a strange place and you see a jar of blue goo near you.',
+        options: [
+            {
+                text: 'Take the goo',
+                setState: { blueGoo: true },
+                nextText: 2
+            },
+            {
+                text: 'Leave the goo',
+                nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+        options: [
+            {
+                text: 'Trade the goo for a sword',
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, sword: true },
+                nextText: 3
+            },
+            {
+                text: 'Trade the goo for a shield',
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, shield: true },
+                nextText: 3
+            },
+            {
+                text: 'Ignore the merchant',
+                nextText: 3
+            }
+        ]
+    },
+    {
+        id: 3,
+        text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+        options: [
+            {
+                text: 'Explore the castle',
+                nextText: 4
+            },
+            {
+                text: 'Find a room to sleep at in the town',
+                nextText: 5
+            },
+            {
+                text: 'Find some hay in a stable to sleep in',
+                nextText: 6
+            }
+        ]
+    },
+    {
+        id: 4,
+        text: 'You are so tired that you fall asleep while exploring the castle and are killed by some terrible monster in your sleep.',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 5,
+        text: 'Without any money to buy a room you break into the nearest inn and fall asleep. After a few hours of sleep the owner of the inn finds you and has the town guard lock you in a cell.',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 6,
+        text: 'You wake up well rested and full of energy ready to explore the nearby castle.',
+        options: [
+            {
+                text: 'Explore the castle',
+                nextText: 7
+            }
+        ]
+    },
+    {
+        id: 7,
+        text: 'While exploring the castle you come across a horrible monster in your path.',
+        options: [
+            {
+                text: 'Try to run',
+                nextText: 8
+            },
+            {
+                text: 'Attack it with your sword',
+                requiredState: (currentState) => currentState.sword,
+                nextText: 9
+            },
+            {
+                text: 'Hide behind your shield',
+                requiredState: (currentState) => currentState.shield,
+                nextText: 10
+            },
+            {
+                text: 'Throw the blue goo at it',
+                requiredState: (currentState) => currentState.blueGoo,
+                nextText: 11
+            }
+        ]
+    },
+    {
+        id: 8,
+        text: 'Your attempts to run are in vain and the monster easily catches.',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 9,
+        text: 'You foolishly thought this monster could be slain with a single sword.',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 10,
+        text: 'The monster laughed as you hid behind your shield and ate you.',
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 11,
+        text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+        options: [
+            {
+                text: 'Congratulations. Play Again.',
+                nextText: -1
+            }
+        ]
+    }
+]
+
+startGame()
